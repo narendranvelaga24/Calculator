c542bbf4680612f70ad2e1fcf87c7b8b4d76edab
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-05-02 00:38:14 +0530

    Create variables and an operate() function

--- script.js
+++ script.js
@@ -8,5 +8,18 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
+    if (b === 0) return "🫥";
     return a / b;
 }
+
+let num1 = 3, num2 = 5, operator = '+';
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: return null;
+    }
+}
+operate(operator, num1, num2);
